afdb9dd2cfb4bf831daa9cab1df0c2e4c1db8ebe
Shreyam Maity <sm8967724231@gmail.com>
2023-04-25 08:14:06 +0530

    fix:list api on resolver

--- src/pages/api/docker/logs.ts
+++ src/pages/api/docker/logs.ts
@@ -16,31 +16,30 @@ export default async function handler(
   let containers = await docker.listCTFContainers();
   let container = docker.docker.getContainer(containers[0].Id);
   const logs: ContainerLogs[] = [];
-  containers.map(async (container) => {
-    let containerData: ContainerLogs;
+  const logPromises = containers.map(async (container) => {
     let container_data = docker.docker.getContainer(container.Id);
     let execResp = await container_data.exec({
       Cmd: ["who"],
       AttachStdout: true,
       AttachStderr: true,
-
     });
     let exec = docker.docker.getExec(execResp.id);
     let stream = await exec.start();
     let data = [];
     stream.on("data", (chunk) => {
       data.push(chunk.toString("utf8"));
-    }
-    );
-    stream.on("end", () => {
-      console.log("end")
-      logs.push({
-        id: container.Id,
-        logs: data,
-      });
-      res.status(200).json({
-        logs: logs,
+    });
+    return new Promise((resolve) => {
+      stream.on("end", () => {
+        logs.push({
+          id: container.Id,
+          logs: data,
+        });
+        resolve();
       });
     });
-  });  
+  });
+
+  await Promise.all(logPromises);
+  res.status(200).json({ logs: logs });
 }
